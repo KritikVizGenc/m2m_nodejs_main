f34610e26144173474ec51b3c30c2c7a64afd111
brknkosuncu <bkosuncu@gmail.com>
2022-07-29 11:32:32 +0300

    Update getMethods.js

--- src/api/getMethods.js
+++ src/api/getMethods.js
@@ -11,6 +11,16 @@ router.get('/getAll', async (req, res) => {
     res.json(users);
 })
 
+
+router.get('/getAll/:id/limit=:side/offset=:offsets', async (req, res) => {
+
+    
+    const users = await User.findAll({where: { user_role: req.params.id}, offset:req.params.offsets, limit: req.params.side });
+    res.json(users);
+})
+
+
+
 router.get('/getById/:id', async (req, res) => {
 
     const user = await User.findAll({ where: { id: req.params.id } })
@@ -18,9 +28,18 @@ router.get('/getById/:id', async (req, res) => {
 })
 
 
-router.get('/getByRole/:roleId', async (req, res) => {
+router.get('/getByRole/:id', async (req, res) => {
 
-    const user = await Role.findAll({ where: { user_role: req.params.roleId } })
+    const user = await Role.findAll({ where: { user_role: req.params.id } })
+
+    
+    res.json(user);
+
+})
+
+router.get('/getByRole/:id/:name', async (req, res) => {
+
+    const user = await Role.findAll({ where:{user_role: req.params.id , name: req.params.name }},)
     res.json(user);
 })
 
